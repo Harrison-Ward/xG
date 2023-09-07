935e2aa1557d9e94a6e81da5757b0c69f344814b
Harrison Ward <hward4116@gmail.com>
2023-09-07 15:59:22 -0600

    added additional checks to prevent duplicates and updated the axis name

--- models/event_scraper.py
+++ models/event_scraper.py
@@ -19,11 +19,137 @@ headers = {
     'Cache-Control': 'max-age=0',
 }
 
+# valid columns to keep from the returned json object
+valid_columns = ['event.tournament.uniqueTournament.hasEventPlayerStatistics',
+                 'event.season.name',
+                 'event.season.year',
+                 'event.season.id',
+                 'event.roundInfo.round',
+                 'event.customId',
+                 'event.status.code',
+                 'event.status.description',
+                 'event.status.type',
+                 'event.winnerCode',
+                 'event.venue.city.name',
+                 'event.venue.stadium.name',
+                 'event.venue.stadium.capacity',
+                 'event.venue.id',
+                 'event.referee.name',
+                 'event.referee.slug',
+                 'event.referee.yellowCards',
+                 'event.referee.redCards',
+                 'event.referee.yellowRedCards',
+                 'event.referee.games',
+                 'event.referee.id',
+                 'event.referee.country.alpha2',
+                 'event.referee.country.name',
+                 'event.homeTeam.name',
+                 'event.homeTeam.slug',
+                 'event.homeTeam.shortName',
+                 'event.homeTeam.userCount',
+                 'event.homeTeam.manager.name',
+                 'event.homeTeam.manager.slug',
+                 'event.homeTeam.manager.id',
+                 'event.homeTeam.manager.country.alpha2',
+                 'event.homeTeam.manager.country.name',
+                 'event.homeTeam.venue.city.name',
+                 'event.homeTeam.venue.stadium.name',
+                 'event.homeTeam.venue.stadium.capacity',
+                 'event.homeTeam.venue.id',
+                 'event.homeTeam.venue.country.alpha2',
+                 'event.homeTeam.venue.country.name',
+                 'event.homeTeam.nameCode',
+                 'event.homeTeam.disabled',
+                 'event.homeTeam.national',
+                 'event.homeTeam.type',
+                 'event.homeTeam.id',
+                 'event.homeTeam.country.alpha2',
+                 'event.homeTeam.country.name',
+                 'event.homeTeam.subTeams',
+                 'event.homeTeam.fullName',
+                 'event.homeTeam.teamColors.primary',
+                 'event.homeTeam.teamColors.secondary',
+                 'event.homeTeam.teamColors.text',
+                 'event.homeTeam.foundationDateTimestamp',
+                 'event.awayTeam.name',
+                 'event.awayTeam.slug',
+                 'event.awayTeam.shortName',
+                 'event.awayTeam.userCount',
+                 'event.awayTeam.manager.name',
+                 'event.awayTeam.manager.slug',
+                 'event.awayTeam.manager.id',
+                 'event.awayTeam.manager.country.alpha2',
+                 'event.awayTeam.manager.country.name',
+                 'event.awayTeam.venue.city.name',
+                 'event.awayTeam.venue.stadium.name',
+                 'event.awayTeam.venue.stadium.capacity',
+                 'event.awayTeam.venue.id',
+                 'event.awayTeam.venue.country.alpha2',
+                 'event.awayTeam.venue.country.name',
+                 'event.awayTeam.nameCode',
+                 'event.awayTeam.type',
+                 'event.awayTeam.id',
+                 'event.awayTeam.country.alpha2',
+                 'event.awayTeam.country.name',
+                 'event.awayTeam.subTeams',
+                 'event.awayTeam.fullName',
+                 'event.awayTeam.teamColors.primary',
+                 'event.awayTeam.teamColors.secondary',
+                 'event.awayTeam.teamColors.text',
+                 'event.awayTeam.foundationDateTimestamp',
+                 'event.homeScore.current',
+                 'event.homeScore.display',
+                 'event.homeScore.period1',
+                 'event.homeScore.period2',
+                 'event.homeScore.normaltime',
+                 'event.awayScore.current',
+                 'event.awayScore.display',
+                 'event.awayScore.period1',
+                 'event.awayScore.period2',
+                 'event.awayScore.normaltime',
+                 'event.time.injuryTime1',
+                 'event.time.injuryTime2',
+                 'event.time.currentPeriodStartTimestamp',
+                 'event.changes.changes',
+                 'event.changes.changeTimestamp',
+                 'event.hasGlobalHighlights',
+                 'event.hasXg',
+                 'event.hasEventPlayerStatistics',
+                 'event.hasEventPlayerHeatMap',
+                 'event.detailId',
+                 'event.crowdsourcingDataDisplayEnabled',
+                 'event.id',
+                 'event.defaultPeriodCount',
+                 'event.defaultPeriodLength',
+                 'event.currentPeriodStartTimestamp',
+                 'event.startTimestamp',
+                 'event.slug',
+                 'event.finalResultOnly',
+                 'event.fanRatingEvent']
+
 headers["If-Modified-Since"] = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S")
 
 
 def compile_events(competition_id_head='11352', competition='premier-league'):
-    """A function desgined to collect all match data for events in the specified competition"""
+    """
+    Collects match data for events in the specified competition.
+
+    Parameters:
+    competition_id_head (str, optional): The competition ID prefix. Default is '11352'.
+    competition (str, optional): The competition name or slug. Default is 'premier-league'.
+
+    Returns:
+    None
+
+    This function retrieves match data for events in the specified competition using the SofaScore API.
+    It generates event IDs, checks their validity, and compiles valid events into a DataFrame.
+    The resulting DataFrame is filtered to include only specified columns ('valid_columns') and saved
+    to a CSV file named '23_24_premier_league_events.csv' in the '../datasets/' directory.
+
+    Note:
+    - The function relies on external variables 'headers' and 'valid_columns' to be defined.
+    - The default values for 'competition_id_head' and 'competition' are set for Premier League data.
+    """
 
     event_id_stubs = []
     for i in range(1000):
@@ -39,49 +165,88 @@ def compile_events(competition_id_head='11352', competition='premier-league'):
         event_id_response_codes.append(tuple((event_id, requests.get(
             f'https://api.sofascore.com/api/v1/event/{competition_id_head}{event_id}', headers=headers).json())))
 
+    # valid_events, invalid_events = [], []
+    # for event_id, event_json in event_id_response_codes:
+    #     try:
+    #         if event_json['event']['tournament']['slug'] == competition:
+    #             valid_events.append(tuple((event_id, event_json)))
+    #     except KeyError:
+    #         invalid_events.append(tuple((event_id, event_json)))
     valid_events, invalid_events = [], []
     for event_id, event_json in event_id_response_codes:
         try:
-            if event_json['event']['tournament']['slug'] == competition:
-                valid_events.append(tuple((event_id, event_json)))
+            if event_json['event']['tournament']['slug'] == competition and event_json['event']['awayTeam']['country']['name'] == 'England':
+                valid_events.append(
+                    tuple((event_json['event']['event.id'], event_json)))
         except KeyError:
             invalid_events.append(tuple((event_id, event_json)))
 
-    for event_id, event_json in valid_events:
-        event_json['event_id'] = f'{competition_id_head}{event_id}'
-
-    premier_league_events = [event_json for event_id, event_json in valid_events]
-
-    valid_columns = "event_id, event.tournament.uniqueTournament.hasEventPlayerStatistics, event.season.name, event.season.year, event.season.id, event.roundInfo.round, event.customId, event.status.code, event.status.description, event.status.type, event.winnerCode, event.attendance, event.venue.city.name, event.venue.stadium.name, event.venue.stadium.capacity, event.venue.id, event.referee.name, event.referee.slug, event.referee.yellowCards, event.referee.redCards, event.referee.yellowRedCards, event.referee.games, event.referee.id, event.referee.country.alpha2, event.referee.country.name, event.homeTeam.name, event.homeTeam.slug, event.homeTeam.shortName, event.homeTeam.userCount, event.homeTeam.manager.name, event.homeTeam.manager.slug, event.homeTeam.manager.id, event.homeTeam.manager.country.alpha2, event.homeTeam.manager.country.name, event.homeTeam.venue.city.name, event.homeTeam.venue.stadium.name, event.homeTeam.venue.stadium.capacity, event.homeTeam.venue.id, event.homeTeam.venue.country.alpha2, event.homeTeam.venue.country.name, event.homeTeam.nameCode, event.homeTeam.disabled, event.homeTeam.national, event.homeTeam.type, event.homeTeam.id, event.homeTeam.country.alpha2, event.homeTeam.country.name, event.homeTeam.subTeams, event.homeTeam.fullName, event.homeTeam.teamColors.primary, event.homeTeam.teamColors.secondary, event.homeTeam.teamColors.text, event.homeTeam.foundationDateTimestamp, event.awayTeam.name, event.awayTeam.slug, event.awayTeam.shortName, event.awayTeam.userCount, event.awayTeam.manager.name, event.awayTeam.manager.slug, event.awayTeam.manager.id, event.awayTeam.manager.country.alpha2, event.awayTeam.manager.country.name, event.awayTeam.venue.city.name, event.awayTeam.venue.stadium.name, event.awayTeam.venue.stadium.capacity, event.awayTeam.venue.id, event.awayTeam.venue.country.alpha2, event.awayTeam.venue.country.name, event.awayTeam.nameCode, event.awayTeam.type, event.awayTeam.id, event.awayTeam.country.alpha2, event.awayTeam.country.name, event.awayTeam.subTeams, event.awayTeam.fullName, event.awayTeam.teamColors.primary, event.awayTeam.teamColors.secondary, event.awayTeam.teamColors.text, event.awayTeam.foundationDateTimestamp, event.homeScore.current, event.homeScore.display, event.homeScore.period1, event.homeScore.period2, event.homeScore.normaltime, event.awayScore.current, event.awayScore.display, event.awayScore.period1, event.awayScore.period2, event.awayScore.normaltime, event.time.injuryTime1, event.time.injuryTime2, event.time.currentPeriodStartTimestamp, event.changes.changes, event.changes.changeTimestamp, event.hasGlobalHighlights, event.hasXg, event.hasEventPlayerStatistics, event.hasEventPlayerHeatMap, event.detailId, event.crowdsourcingDataDisplayEnabled, event.id, event.defaultPeriodCount, event.defaultPeriodLength, event.currentPeriodStartTimestamp, event.startTimestamp, event.slug, event.finalResultOnly, event.fanRatingEvent, event.homeRedCards, event.awayRedCards".split(
-        ', ')
+    premier_league_events = [event[1] for event in valid_events]
 
     premier_league_df = pd.json_normalize(premier_league_events)
     premier_league_df = premier_league_df[valid_columns]
     premier_league_df.to_csv('../datasets/23_24_premier_league_events.csv')
 
 
-def refresh_events(filepath, competition_id_head='11352', competition='premier-league'):
-    """Function to check the status of pre-fetched events in the events dataframe"""
-    
-    updated_event_info, new_completed_events = [], []
-    # load the precompiled events df and check which events still haven't been completed
-    event_df = pd.read_csv(filepath)
-    uncompleted_events = event_df['event.id'][event_df['event.status.type']=='notstarted'].values
+def update_event_df(filepath: str, headers=headers, valid_columns=None):
+    """
+    Updates an event DataFrame with new information from an external API.
+
+    Parameters:
+    filepath (str): The file path to the CSV containing the event data.
+    valid_columns(list): list of columns in the target csv
+    headers: list of headers from the users machine used to propogate the api request
+
+    Returns:
+    None
+
+    This function reads an event DataFrame from a CSV file located at 'filepath'.
+    It then checks for new events that have not started yet, fetches their updated JSON data
+    from an API, and identifies events that have been marked as 'finished'.
+    The completed event information is appended to the DataFrame, and rows with out-of-date
+    data are dropped. Finally, the updated DataFrame is saved back to the CSV file.
+
+    Note:
+    - The 'filepath' should point to a CSV file with the following columns:
+      - 'event.id': Unique identifier for each event.
+      - 'event.status.type': Status of the event, e.g., 'notstarted' or 'finished'.
+      - Other columns as needed for event data.
+    - The function relies on external variables 'headers' and 'valid_columns' to be defined.
+    """
+    # init lists to hold updated event info
+    updated_event_info, new_completed_events, new_completed_events_ids = [], [], []
+
+    # follow the laziest possible procedure to check for new events, check every unstarted event
+    event_df = pd.read_csv(filepath, index_col='Unnamed: 0')
+    uncompleted_events = event_df['event.id'][event_df['event.status.type']
+                                              == 'notstarted'].values
+
+    if valid_columns is None:
+        valid_columns = event_df.columns
 
     # fetch updated json data for the uncompleted events
     for event_id in uncompleted_events:
         updated_event_info.append(requests.get(
-            f'https://api.sofascore.com/api/v1/event/{competition_id_head}{event_id}', headers=headers).json())
+            f'https://api.sofascore.com/api/v1/event/{event_id}', headers=headers).json())
 
-    # check if the event has been completed 
+    # check if the event has been completed
     for event in updated_event_info:
         if event['event']['status']['type'] == 'finished':
             new_completed_events.append(event)
-    
-    # replace the data in event df with new data
-    for event in updated_event_info:
-        event_id = event['event']['event.id']
 
-        
+    # append the updated json info to the end of the df
+    for event in new_completed_events:
+        new_completed_events_ids.append(event['event']['id'])
 
-    
+    # drop the rows in the dataframe that have the out of date data
+    event_df = event_df[~event_df['event.id'].isin(new_completed_events_ids)]
+
+    # append the updated data to the df and save
+    event_df = pd.concat([event_df, pd.json_normalize(
+        new_completed_events)[valid_columns]], sort=True)
+    event_df.to_csv(filepath)
+
+
+if __name__ == "__main__":
+    update_event_df(
+        '/Users/harrisonward/Desktop/CS/Git/xG/datasets/23_24_premier_league_events.csv')
--- models/shotmap_extractor.py
+++ models/shotmap_extractor.py
@@ -18,46 +18,81 @@ headers = {
 
 
 def shotmap_extractor(event_filepath, shotmap_filepath):
+    """
+    Extracts and updates shotmap data from SofaScore API and merges it with event information.
+
+    Parameters:
+    - event_filepath (str): Filepath to the CSV file containing event data.
+    - shotmap_filepath (str): Filepath to the CSV file containing shotmap data.
+
+    The function performs the following steps:
+    1. Loads the event data from the provided CSV file.
+    2. Filters and extracts completed event IDs.
+    3. Merges relevant event information (home and away team, match details) into a DataFrame.
+    4. Checks if the event IDs are already present in the shotmap DataFrame.
+    5. Retrieves shotmap data from the SofaScore API for new events.
+    6. Integrates shotmap data with event details.
+    7. Determines the team the shooter plays for and their opponent.
+    8. Saves the updated shotmap data to the specified CSV file.
+
+    Note: The function relies on external data sources and assumes specific column names in the CSV files.
+
+    """
     # load the event df and the list of finished events
     event_df = pd.read_csv(event_filepath)
     completed_event_ids = event_df['event.id'][event_df['event.status.type']
                                                == 'finished'].values
     
+    
+
     # merge in home and away team and match slug
     event_info_df = event_df[['event.awayTeam.slug', 'event.homeTeam.slug',
                               'event.slug', 'event.winnerCode', 'event.awayTeam.name', 'event.homeTeam.name', 'event.id']]
 
     # check if event id is already in the shot map dataframe
-    shotmap_df = pd.read_csv(shotmap_filepath)
-    completed_shotmap_event_ids = shotmap_df['event.id'].values
+    shotmap_df = pd.read_csv(shotmap_filepath, index_col='Unnamed: 0')
+    completed_shotmap_event_ids_set = set(shotmap_df['event.id'].values)
 
     # pull shotmap data from the web
     new_shotmaps = []
     for event_id in completed_event_ids:
-        if event_id not in completed_shotmap_event_ids:
+        if event_id not in completed_shotmap_event_ids_set:
+            print(f"Processing event_id: {event_id}")
             new_shotmaps.append(requests.get(
                 f'https://api.sofascore.com/api/v1/event/{event_id}/shotmap', headers=headers).json())
 
-    # add event.id and match data to the shotmap
-    unpacked_shotmap_dfs = []
+    # add event.id, match data, and binary vars to the shotmap
     for i, shot_maps in enumerate(new_shotmaps):
-        try:
-            temp_df = pd.json_normalize(shot_maps['shotmap'])
-            temp_df['event.id'] = completed_event_ids[i]
-            temp_df = temp_df.merge(event_info_df, how='left', left_on='event.id', right_on='event.id')
-            shotmap_df = pd.concat([shotmap_df, temp_df], sort=True)
-        except KeyError:
-            pass
+        if completed_event_ids[i] not in completed_shotmap_event_ids_set:
+            try:
+                # merge in event data from the events df
+                temp_df = pd.json_normalize(shot_maps['shotmap'])
+                temp_df['event.id'] = completed_event_ids[i]
+                temp_df = temp_df.merge(
+                    event_info_df, how='left', left_on='event.id', right_on='event.id')
 
+                # create binary variables from the event data
+                temp_df = pd.concat([temp_df, pd.get_dummies(
+                    temp_df.situation, prefix='shot_sit')], axis=1)
+                temp_df = pd.concat([temp_df, pd.get_dummies(
+                    temp_df.isHome, prefix='homeTeam')], axis=1)
+                temp_df = pd.concat([temp_df, pd.get_dummies(
+                    temp_df.shotType, prefix='outcome')], axis=1)
+                temp_df = pd.concat([temp_df, pd.get_dummies(
+                    temp_df.bodyPart, prefix='bodyPart')], axis=1)
 
-    # drop the index column to avoid redundancy
-    shotmap_df = shotmap_df.drop('Unnamed: 0', axis=1)
-    
+                # determine the team the shooter plays for and their opponent
+                temp_df['team'] = np.where(
+                    temp_df['isHome'], temp_df['event.homeTeam.name'], temp_df['event.awayTeam.name'])
+                temp_df['opponent'] = np.where(
+                    temp_df['isHome'] == False, temp_df['event.homeTeam.name'], temp_df['event.awayTeam.name'])
 
-    # determine the team the shooter plays for and their opponent
-    shotmap_df['team'] = np.where(shotmap_df['isHome'], shotmap_df['event.homeTeam.slug'], shotmap_df['event.awayTeam.slug'])
-    shotmap_df['opponent'] = np.where(shotmap_df['isHome']==False, shotmap_df['event.homeTeam.slug'], shotmap_df['event.awayTeam.slug'])
-
+                # add the new temporary df to the shotmap data
+                print(completed_event_ids[i])
+                shotmap_df = pd.concat([shotmap_df, temp_df], sort=True, verify_integrity=True, ignore_index=True)
+            except KeyError:
+                print(
+                    f'\nWarning: polluted data\nPolluted Event Id:{completed_event_ids[i]}')
 
     # save updated shotmap csv
     shotmap_df.to_csv(shotmap_filepath)
