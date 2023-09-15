d949e8833e500866e0ed2c99434fa05502c3b184
Harrison Ward <hward4116@gmail.com>
2023-09-15 01:06:00 -0700

    added duplicate entry check

--- scripts/augmented_shotmap_compilier.py
+++ scripts/augmented_shotmap_compilier.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import logging
 
 
@@ -26,13 +27,31 @@ def augmented_shotmap_compilier(augmented_shotmap_filepath, player_event_filepat
     augmented_shotmap_df = pd.read_csv(
         augmented_shotmap_filepath, index_col='Unnamed: 0')
 
+    new_player_event_ids = np.unique(
+        player_df[['player.id', 'event.id']].values, axis=0)
+    stored_player_event_ids = np.unique(
+        augmented_shotmap_df[['player.id', 'event.id']], axis=0)
+
+    # check if any rows are not in the shotmap dataframe
+    stored_player_event_ids_set = [tuple(row)
+                                   for row in stored_player_event_ids]
+
+    # check the difference between the two set lists
+    new_player_event_ids = np.array([row for row in new_player_event_ids if tuple(
+        row) not in stored_player_event_ids_set])
+
     # take the data we are interested in from the player dataframe
-    player_match_data = player_df[[
-        'event.id', 'player.id', 'statistics.minutesPlayed', 'statistics.expectedAssists', 'player.position']]
+    player_match_data = player_df.loc[(player_df['player.id'].isin(new_player_event_ids)) & (player_df['event.id'].isin(
+        new_player_event_ids)), ['event.id', 'player.id', 'statistics.minutesPlayed', 'statistics.expectedAssists', 'player.position']]
+
+    if player_match_data.shape[0] == 0:
+        logging.warning('WARNING: No new player event data to merge')
+        logging.info('Augmented Shotmap Compilier successfully exited\n\n\n')
+        return 0
 
     # merge the player data on the event and player id
     augmented_shotmap_df = augmented_shotmap_df.merge(right=player_match_data, how='left', left_on=[
-                                                      'event.id', 'player.id'], right_on=['event.id', 'player.id'])
+        'event.id', 'player.id'], right_on=['event.id', 'player.id'])
 
     # save the augmented shotmap dataframe
     augmented_shotmap_df.to_csv(augmented_shotmap_filepath)
