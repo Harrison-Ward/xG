aca9b66dc80af00186b45475cae1b11b9041ffa5
Harrison Ward <hward4116@gmail.com>
2023-09-15 00:57:58 -0700

    Auto-commit changes to the datasets

--- scripts/augmented_shotmap_compilier.py
+++ scripts/augmented_shotmap_compilier.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import logging
 
+
 def augmented_shotmap_compilier(augmented_shotmap_filepath, player_event_filepath):
     """
     Compile augmented shotmap data with player event statistics and save the result.
@@ -22,15 +23,18 @@ def augmented_shotmap_compilier(augmented_shotmap_filepath, player_event_filepat
     logging.info('Augmented Shotmap compilier started...')
     # read in the augmented shotmap and player events dataframe to make merges
     player_df = pd.read_csv(player_event_filepath, index_col='Unnamed: 0')
-    augmented_shotmap_df = pd.read_csv(augmented_shotmap_filepath, index_col='Unnamed: 0')
+    augmented_shotmap_df = pd.read_csv(
+        augmented_shotmap_filepath, index_col='Unnamed: 0')
 
     # take the data we are interested in from the player dataframe
-    player_match_data = player_df[['event.id', 'player.id', 'statistics.minutesPlayed', 'statistics.expectedAssists', 'player.position']]
+    player_match_data = player_df[[
+        'event.id', 'player.id', 'statistics.minutesPlayed', 'statistics.expectedAssists', 'player.position']]
 
     # merge the player data on the event and player id
-    augmented_shotmap_df = augmented_shotmap_df.merge(right=player_match_data, how='left', left_on=['event.id', 'player.id'], right_on=['event.id', 'player.id'])
+    augmented_shotmap_df = augmented_shotmap_df.merge(right=player_match_data, how='left', left_on=[
+                                                      'event.id', 'player.id'], right_on=['event.id', 'player.id'])
 
-    # save the augmented shotmap dataframe 
+    # save the augmented shotmap dataframe
     augmented_shotmap_df.to_csv(augmented_shotmap_filepath)
     logging.info('Augmented Shotmap compilier sucessfully exited...')
     return 0
